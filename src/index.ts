7f7bda04c3eb74d06c6f3a81a9006b10dd70d1de
Inian Parameshwaran <inian1234@gmail.com>
2021-02-23 04:06:51 +0530

    storage: upload object working

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ import fastify, { FastifyInstance } from 'fastify'
 import { Server, IncomingMessage, ServerResponse } from 'http'
 import autoload from 'fastify-autoload'
 import path from 'path'
+import fastifyMultipart from 'fastify-multipart'
 
 const app: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({
   logger: true,
 })
+
+// @todo - should we set upload limits here?
+// https://github.com/fastify/fastify-multipart#handle-file-size-limitation
+app.register(fastifyMultipart)
 
 app.register(autoload, {
   dir: path.join(__dirname, 'routes'),
--- src/routes/object.ts
+++ src/routes/object.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance, RequestGenericInterface } from 'fastify'
 import { PostgrestClient } from '@supabase/postgrest-js'
 import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { Upload } from '@aws-sdk/lib-storage'
 import dotenv from 'dotenv'
 import jwt from 'jsonwebtoken'
 
@@ -15,7 +16,20 @@ const {
   JWT_SECRET: jwtSecret,
 } = process.env
 
-const client = new S3Client({ region })
+// @todo define as an interface expecting sub instead
+type jwtType =
+  | {
+      aud: string
+      exp: number
+      sub: string
+      email: string
+      app_metadata: object
+      user_metadata: object
+      role: string
+    }
+  | undefined
+
+const client = new S3Client({ region, runtime: 'node' })
 interface requestGeneric extends RequestGenericInterface {
   Params: {
     bucketName: string
@@ -44,6 +58,9 @@ type Object = {
 }
 
 function getPostgrestClient(jwt: string) {
+  if (!anonKey) {
+    throw new Error('anonKey not found')
+  }
   // @todo in kps, can we just ping localhost?
   const url = `https://${projectRef}.${supabaseDomain}/rest/v1`
   const postgrest = new PostgrestClient(url, {
@@ -55,7 +72,10 @@ function getPostgrestClient(jwt: string) {
   return postgrest
 }
 
-function verifyJWT(token: string) {
+function verifyJWT(token: string): Promise<object | undefined> {
+  if (!jwtSecret) {
+    throw new Error('no jwtsecret')
+  }
   return new Promise((resolve, reject) => {
     jwt.verify(token, jwtSecret, (err, decoded) => {
       if (err) return reject(err)
@@ -66,7 +86,7 @@ function verifyJWT(token: string) {
 
 async function getOwner(token: string) {
   const decodedJWT = await verifyJWT(token)
-  return decodedJWT.sub
+  return (decodedJWT as jwtType)?.sub
 }
 
 export default async function routes(fastify: FastifyInstance) {
@@ -110,6 +130,7 @@ export default async function routes(fastify: FastifyInstance) {
     const data = await client.send(command)
     console.log('done s3')
 
+    // @todo stream the response back instead of awaiting from s3 and sending back
     return response
       .status(data.$metadata.httpStatusCode ?? 200)
       .header('content-type', data.ContentType)
@@ -129,20 +150,47 @@ export default async function routes(fastify: FastifyInstance) {
     const objectName = request.params['*']
 
     const postgrest = getPostgrestClient(jwt)
-    const owner = getOwner(jwt)
+    const owner = await getOwner(jwt)
+    // @todo how to merge these into one query?
+    // i can create a view and add INSTEAD OF triggers..is that the way to do it?
+    // @todo add unique constraint for just bucket names
+    const { data: bucket, error: bucketError } = await postgrest
+      .from('buckets')
+      .select('id')
+      .eq('name', bucketName)
+      .single()
+    if (bucketError) throw bucketError
+    console.log(bucket)
+
     const { data: results, error } = await postgrest
       .from<Object>('objects')
       .insert([
         {
           name: objectName,
-          owner: 'xxx', // decode jwt to get the owner
-          bucketId: 'xxx', // do i need to make an api call for this? create a view
+          owner: owner,
+          bucketId: bucket.id,
         },
       ])
       .single()
+    console.log(results, error)
 
     // if successfully inserted, upload to s3
+    const s3Key = `${projectRef}/${bucketName}/${objectName}`
+    const data = await request.file()
+
+    const paralellUploads3 = new Upload({
+      client,
+      params: {
+        Bucket: globalS3Bucket,
+        Key: s3Key,
+        Body: data.file,
+      },
+    })
+
+    await paralellUploads3.done()
+
     // @todo return metadata of the uploaded object
+    return response.status(200).send('Uploaded')
   })
 
   fastify.delete('/object/:bucketId/:objectId', async (request, reply) => {})

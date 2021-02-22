d5a4b4f512cd667d7c58270a3c49317a23033077
Inian Parameshwaran <inian1234@gmail.com>
2021-02-22 06:31:21 +0530

    storage: upload object WIP

--- src/routes/object.ts
+++ src/routes/object.ts
@@ -2,6 +2,7 @@ import { FastifyInstance, RequestGenericInterface } from 'fastify'
 import { PostgrestClient } from '@supabase/postgrest-js'
 import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import dotenv from 'dotenv'
+import jwt from 'jsonwebtoken'
 
 dotenv.config()
 
@@ -10,8 +11,11 @@ const {
   PROJECT_REF: projectRef,
   BUCKET_NAME: globalS3Bucket,
   SUPABASE_DOMAIN: supabaseDomain,
+  ANON_KEY: anonKey,
+  JWT_SECRET: jwtSecret,
 } = process.env
 
+const client = new S3Client({ region })
 interface requestGeneric extends RequestGenericInterface {
   Params: {
     bucketName: string
@@ -35,32 +39,48 @@ type Object = {
   createdAt: string
   updatedAt: string
   lastAccessedAt: string
-  metadata: string
-  buckets: Bucket
+  metadata?: string
+  buckets?: Bucket
+}
+
+function getPostgrestClient(jwt: string) {
+  // @todo in kps, can we just ping localhost?
+  const url = `https://${projectRef}.${supabaseDomain}/rest/v1`
+  const postgrest = new PostgrestClient(url, {
+    headers: {
+      apiKey: anonKey,
+      Authorization: `Bearer ${jwt}`,
+    },
+  })
+  return postgrest
+}
+
+function verifyJWT(token: string) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, jwtSecret, (err, decoded) => {
+      if (err) return reject(err)
+      resolve(decoded)
+    })
+  })
+}
+
+async function getOwner(token: string) {
+  const decodedJWT = await verifyJWT(token)
+  return decodedJWT.sub
 }
 
 export default async function routes(fastify: FastifyInstance) {
   fastify.get<requestGeneric>('/object/:bucketName/*', async (request, response) => {
     const authHeader = request.headers.authorization
-    const anonKey = process.env.ANON_KEY
-
     if (!authHeader || !anonKey) {
       return response.status(403).send('Go away')
     }
     const jwt = authHeader.substring('Bearer '.length)
 
-    // @todo in kps, can we just ping localhost?
-    const url = `https://${projectRef}.${supabaseDomain}/rest/v1`
-    const postgrest = new PostgrestClient(url)
-    postgrest.headers = {
-      apiKey: anonKey,
-    }
-    postgrest.auth(jwt)
+    const postgrest = getPostgrestClient(jwt)
 
     const { bucketName } = request.params
     const objectName = request.params['*']
-
-    console.log(bucketName, objectName)
 
     const { data: results, error } = await postgrest
       .from<Object>('objects')
@@ -72,7 +92,7 @@ export default async function routes(fastify: FastifyInstance) {
       .single()
 
     console.log(error)
-    console.log(results)
+    // console.log(results)
     if (!results?.buckets) {
       // @todo why is this check necessary?
       // if corresponding bucket is not found, i want the object also to not be returned
@@ -83,19 +103,47 @@ export default async function routes(fastify: FastifyInstance) {
     // send the object from s3
     const s3Key = `${projectRef}/${bucketName}/${objectName}`
     console.log(s3Key)
-    const client = new S3Client({ region })
     const command = new GetObjectCommand({
       Bucket: globalS3Bucket,
       Key: s3Key,
     })
     const data = await client.send(command)
-    console.log(data)
+    console.log('done s3')
 
     return response
       .status(data.$metadata.httpStatusCode ?? 200)
       .header('content-type', data.ContentType)
       .send(data.Body)
   })
-  fastify.post('/object/:bucketId/:objectId', async (request, reply) => {})
+
+  fastify.post<requestGeneric>('/object/:bucketName/*', async (request, response) => {
+    // @todo should upsert work?
+    // check if the user is able to insert that row
+    const authHeader = request.headers.authorization
+    if (!authHeader || !anonKey) {
+      return response.status(403).send('Go away')
+    }
+    const jwt = authHeader.substring('Bearer '.length)
+
+    const { bucketName } = request.params
+    const objectName = request.params['*']
+
+    const postgrest = getPostgrestClient(jwt)
+    const owner = getOwner(jwt)
+    const { data: results, error } = await postgrest
+      .from<Object>('objects')
+      .insert([
+        {
+          name: objectName,
+          owner: 'xxx', // decode jwt to get the owner
+          bucketId: 'xxx', // do i need to make an api call for this? create a view
+        },
+      ])
+      .single()
+
+    // if successfully inserted, upload to s3
+    // @todo return metadata of the uploaded object
+  })
+
   fastify.delete('/object/:bucketId/:objectId', async (request, reply) => {})
 }

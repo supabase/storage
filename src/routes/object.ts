7db0a69f9b913a2a9cc3550936c2acde84e83bce
Inian Parameshwaran <inian1234@gmail.com>
2021-02-23 06:20:14 +0530

    storage: implement put object

--- src/routes/object.ts
+++ src/routes/object.ts
@@ -196,6 +196,65 @@ export default async function routes(fastify: FastifyInstance) {
     return response.status(200).send('Uploaded')
   })
 
+  // @todo add content-type metadata properly on upload and put
+  // so that getobject sends the content-type header correctly
+  fastify.put<requestGeneric>('/object/:bucketName/*', async (request, response) => {
+    // check if the user is able to update the row
+    const authHeader = request.headers.authorization
+    if (!authHeader || !anonKey) {
+      return response.status(403).send('Go away')
+    }
+    const jwt = authHeader.substring('Bearer '.length)
+
+    const { bucketName } = request.params
+    const objectName = request.params['*']
+
+    const postgrest = getPostgrestClient(jwt)
+    const owner = await getOwner(jwt)
+    // @todo how to merge these into one query?
+    // i can create a view and add INSTEAD OF triggers..is that the way to do it?
+    // @todo add unique constraint for just bucket names
+    const { data: bucket, error: bucketError } = await postgrest
+      .from('buckets')
+      .select('id')
+      .eq('name', bucketName)
+      .single()
+    if (bucketError) throw bucketError
+    console.log(bucket)
+
+    const { data: results, error } = await postgrest
+      .from<Object>('objects')
+      .update({
+        lastAccessedAt: new Date().toISOString(),
+      })
+      .match({ bucketId: bucket.id, name: objectName })
+      .limit(1)
+
+    console.log('results: ', results)
+    console.log('error: ', error)
+    if (error || (results && results.length === 0)) {
+      return response.status(403).send('Go away')
+    }
+
+    // if successfully inserted, upload to s3
+    const s3Key = `${projectRef}/${bucketName}/${objectName}`
+    const data = await request.file()
+
+    const paralellUploads3 = new Upload({
+      client,
+      params: {
+        Bucket: globalS3Bucket,
+        Key: s3Key,
+        Body: data.file,
+      },
+    })
+
+    await paralellUploads3.done()
+
+    // @todo return metadata of the uploaded object
+    return response.status(200).send('Uploaded')
+  })
+
   fastify.delete<requestGeneric>('/object/:bucketName/*', async (request, response) => {
     // check if the user is able to insert that row
     const authHeader = request.headers.authorization

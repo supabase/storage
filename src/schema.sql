f764b260336af573df2e66d4c957621620d6b4c7
Inian Parameshwaran <inian1234@gmail.com>
2021-02-25 08:13:20 +0530

    storage: add initial schema file

---
+++ src/schema.sql
@@ -0,0 +1,91 @@
+DROP TABLE IF EXISTS "public"."buckets";
+CREATE TABLE "public"."buckets" (
+    "id" uuid NOT NULL,
+    "name" varchar,
+    "owner" uuid,
+    "createdAt" timestamptz DEFAULT now(),
+    "updatedAt" timestamptz DEFAULT now(),
+    CONSTRAINT "buckets_owner_fkey" FOREIGN KEY ("owner") REFERENCES "auth"."users"("id"),
+    PRIMARY KEY ("id")
+);
+
+DROP TABLE IF EXISTS "public"."objects";
+CREATE TABLE "public"."objects" (
+    "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+    "bucketId" uuid,
+    "name" varchar,
+    "owner" uuid,
+    "createdAt" timestamptz DEFAULT now(),
+    "updatedAt" timestamptz DEFAULT now(),
+    "lastAccessedAt" timestamptz DEFAULT now(),
+    "metadata" jsonb,
+    CONSTRAINT "objects_bucketId_fkey" FOREIGN KEY ("bucketId") REFERENCES "public"."buckets"("id"),
+    CONSTRAINT "objects_owner_fkey" FOREIGN KEY ("owner") REFERENCES "auth"."users"("id"),
+    PRIMARY KEY ("id")
+);
+
+
+CREATE OR REPLACE FUNCTION public.foldername(name varchar)
+ RETURNS varchar[]
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+_parts varchar[];
+BEGIN
+	select string_to_array(name, '/') into _parts;
+	return _parts[1:array_length(_parts,1)-1];
+END
+$function$;
+
+CREATE OR REPLACE FUNCTION public.filename(name varchar)
+ RETURNS varchar
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+_parts varchar[];
+BEGIN
+	select string_to_array(name, '/') into _parts;
+	return _parts[array_length(_parts,1)];
+END
+$function$;
+
+CREATE OR REPLACE FUNCTION public.extension(name varchar)
+ RETURNS varchar
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+_parts varchar[];
+_filename varchar;
+BEGIN
+	select string_to_array(name, '/') into _parts;
+	select _parts[array_length(_parts,1)] into _filename;
+	-- return the last part instead of 2
+	return split_part(_filename, '.', 2);
+END
+$function$;
+
+CREATE OR REPLACE FUNCTION public.search(prefix text, bucketname text, limits int DEFAULT 100, levels int DEFAULT 1, offsets int DEFAULT 0)
+ RETURNS TABLE (
+    folder text,
+    id uuid
+  )
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+BEGIN
+	return query 
+		with files_folders as (
+			select ((string_to_array(name, '/'))[levels]) as folder
+			from objects_clone
+			where name like prefix || '%'
+			and "bucketId" in (select buckets."id" from buckets where "name"=bucketname limit 1)
+			GROUP by folder
+			order by folder
+			limit limits
+			offset offsets
+		) 
+		select files_folders.folder as name, objects_clone.id from files_folders
+		left join objects_clone
+		on prefix || files_folders.folder = objects_clone.name;
+END
+$function$;

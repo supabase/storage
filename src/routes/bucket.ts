85aab76905dda59900dc229fa0ca7f72ee7b0fdf
Inian Parameshwaran <inian1234@gmail.com>
2021-02-25 19:03:26 +0530

    storage: implement empty bucket

--- src/routes/bucket.ts
+++ src/routes/bucket.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance, RequestGenericInterface } from 'fastify'
 import dotenv from 'dotenv'
 import { getPostgrestClient, getOwner } from '../utils'
+import { DeleteObjectsCommand, S3Client } from '@aws-sdk/client-s3'
 
 dotenv.config()
 interface requestGeneric extends RequestGenericInterface {
@@ -36,7 +37,15 @@ type Obj = {
   buckets?: Bucket
 }
 
-const { ANON_KEY: anonKey, SERVICE_KEY: serviceKey } = process.env
+const {
+  ANON_KEY: anonKey,
+  SERVICE_KEY: serviceKey,
+  BUCKET_NAME: globalS3Bucket,
+  PROJECT_REF: projectRef,
+  REGION: region,
+} = process.env
+
+const client = new S3Client({ region, runtime: 'node' })
 
 export default async function routes(fastify: FastifyInstance) {
   // @todo I have enabled RLS only for objects table
@@ -76,7 +85,60 @@ export default async function routes(fastify: FastifyInstance) {
   })
 
   fastify.post<requestGeneric>('/bucket/:bucketId/empty', async (request, response) => {
-    // @todo
+    const authHeader = request.headers.authorization
+    if (!authHeader || !anonKey || !serviceKey) {
+      return response.status(403).send('Go away')
+    }
+
+    const jwt = authHeader.substring('Bearer '.length)
+    const { bucketId } = request.params
+    const postgrest = getPostgrestClient(jwt)
+
+    const { data: bucket, error: bucketError } = await postgrest
+      .from<Bucket>('buckets')
+      .select('name')
+      .eq('id', bucketId)
+      .single()
+    console.log(bucket, bucketError)
+    if (!bucket) {
+      return response.status(400).send('Bucket not found')
+    }
+    const bucketName = bucket.name
+
+    // @todo add pagination
+    const { data: objects, error: objectError } = await postgrest
+      .from<Obj>('objects')
+      .select('name, id')
+      .eq('bucketId', bucketId)
+      .limit(1000)
+
+    console.log(objects, objectError)
+    if (objects) {
+      const params = objects.map((ele) => {
+        return {
+          Key: `${projectRef}/${bucketName}/${ele.name}`,
+        }
+      })
+      console.log(params)
+      const command = new DeleteObjectsCommand({
+        Bucket: globalS3Bucket,
+        Delete: {
+          Objects: params,
+        },
+      })
+      await client.send(command)
+
+      const { error: deleteError } = await postgrest
+        .from<Obj>('objects')
+        .delete()
+        .in(
+          'id',
+          objects.map((ele) => ele.id)
+        )
+      console.log(deleteError)
+    }
+
+    return response.status(200).send('Emptied')
   })
 
   fastify.delete<requestGeneric>('/bucket/:bucketId', async (request, response) => {

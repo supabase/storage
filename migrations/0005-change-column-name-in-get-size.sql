25949d5e441807b6b10a35f62ed7a139f7eb62f1
Inian Parameshwaran <inian1234@gmail.com>
2021-03-23 09:22:07 +0530

    fix(get_size): change column name bucket -> bucket_id

---
+++ migrations/0005-change-column-name-in-get-size.sql
@@ -0,0 +1,15 @@
+DROP FUNCTION storage.get_size_by_bucket();
+CREATE OR REPLACE FUNCTION storage.get_size_by_bucket()
+ RETURNS TABLE (
+    size BIGINT,
+    bucket_id text
+  )
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+    return query
+        select sum((metadata->>'size')::int) as size, obj.bucket_id
+        from "storage".objects as obj
+        group by obj.bucket_id;
+END
+$function$;

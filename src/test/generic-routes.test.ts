4f0c98f342c9fb9dcdfe40b8729864d7c316b5e5
Ademílson F. Tonato <ademilsonft@outlook.com>
2021-04-01 10:28:42 +0100

    test: add generic-routes test file

---
+++ src/test/generic-routes.test.ts
@@ -0,0 +1,75 @@
+'use strict'
+
+import { createDefaultSchema, createResponse } from '../utils/generic-routes'
+
+describe('testing Generic Routes Utils', () => {
+  describe('creating generic responses [createResponse]', () => {
+    test('create generic response with all values', () => {
+      const response = {
+        message: 'Invalid schema',
+        statusCode: '400',
+        error: 'Invalid schema error message',
+      }
+
+      expect(createResponse('Invalid schema', '400', 'Invalid schema error message')).toEqual(
+        response
+      )
+    })
+
+    test('create generic response without status', () => {
+      const response = {
+        message: 'Invalid schema',
+        error: 'Invalid schema error message',
+      }
+
+      expect(createResponse('Invalid schema', undefined, 'Invalid schema error message')).toEqual(
+        response
+      )
+    })
+
+    test('create generic response without error', () => {
+      const response = {
+        message: 'Invalid schema',
+        statusCode: '400',
+      }
+
+      expect(createResponse('Invalid schema', '400')).toEqual(response)
+    })
+
+    test('create generic response only message', () => {
+      const response = {
+        message: 'Invalid schema',
+      }
+
+      expect(createResponse('Invalid schema')).toEqual(response)
+    })
+  })
+
+  describe('creating generic schema [createDefaultSchema]', () => {
+    test('create generic schema without additional properties', () => {
+      const successResponseSchema = { generic: 'example' }
+      const response = {
+        schema: {
+          headers: { $ref: 'authSchema#' },
+          response: { 200: successResponseSchema, '4xx': { $ref: 'errorSchema#' } },
+        },
+      }
+
+      expect(createDefaultSchema(successResponseSchema, {})).toEqual(response)
+    })
+
+    test('create generic schema with additional properties', () => {
+      const successResponseSchema = { generic: 'example' }
+      const additionalProperties = { generic: 'example' }
+      const response = {
+        schema: {
+          headers: { $ref: 'authSchema#' },
+          response: { 200: successResponseSchema, '4xx': { $ref: 'errorSchema#' } },
+          ...additionalProperties,
+        },
+      }
+
+      expect(createDefaultSchema(successResponseSchema, additionalProperties)).toEqual(response)
+    })
+  })
+})

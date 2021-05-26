0abd6fe5686f2d8e69c53a5a0f37681be7c4fce7
Inian <inian1234@gmail.com>
2021-05-26 18:17:42 +0800

    runs prettier

--- src/routes/object/getObject.ts
+++ src/routes/object/getObject.ts
@@ -22,48 +22,57 @@ interface getObjectRequestInterface extends AuthenticatedRequest {
   Params: FromSchema<typeof getObjectParamsSchema>
 }
 
-async function requestHandler(request: FastifyRequest<getObjectRequestInterface, Server, IncomingMessage>, response: FastifyReply<Server, IncomingMessage, ServerResponse, getObjectRequestInterface, unknown>) {
+async function requestHandler(
+  request: FastifyRequest<getObjectRequestInterface, Server, IncomingMessage>,
+  response: FastifyReply<
+    Server,
+    IncomingMessage,
+    ServerResponse,
+    getObjectRequestInterface,
+    unknown
+  >
+) {
   const authHeader = request.headers.authorization
-      const jwt = authHeader.substring('Bearer '.length)
+  const jwt = authHeader.substring('Bearer '.length)
 
-      const postgrest = getPostgrestClient(jwt)
+  const postgrest = getPostgrestClient(jwt)
 
-      const { bucketName } = request.params
-      const objectName = request.params['*']
+  const { bucketName } = request.params
+  const objectName = request.params['*']
 
-      if (!isValidKey(objectName) || !isValidKey(bucketName)) {
-        return response
-          .status(400)
-          .send(createResponse('The key contains invalid characters', '400', 'Invalid key'))
-      }
+  if (!isValidKey(objectName) || !isValidKey(bucketName)) {
+    return response
+      .status(400)
+      .send(createResponse('The key contains invalid characters', '400', 'Invalid key'))
+  }
 
-      const objectResponse = await postgrest
-        .from<Obj>('objects')
-        .select('id')
-        .match({
-          name: objectName,
-          bucket_id: bucketName,
-        })
-        .single()
+  const objectResponse = await postgrest
+    .from<Obj>('objects')
+    .select('id')
+    .match({
+      name: objectName,
+      bucket_id: bucketName,
+    })
+    .single()
 
-      if (objectResponse.error) {
-        const { status, error } = objectResponse
-        request.log.error({ error }, 'error object')
-        return response.status(400).send(transformPostgrestError(error, status))
-      }
+  if (objectResponse.error) {
+    const { status, error } = objectResponse
+    request.log.error({ error }, 'error object')
+    return response.status(400).send(transformPostgrestError(error, status))
+  }
 
-      // send the object from s3
-      const s3Key = `${projectRef}/${bucketName}/${objectName}`
-      request.log.info(s3Key)
-      const data = await getObject(client, globalS3Bucket, s3Key)
+  // send the object from s3
+  const s3Key = `${projectRef}/${bucketName}/${objectName}`
+  request.log.info(s3Key)
+  const data = await getObject(client, globalS3Bucket, s3Key)
 
-      return response
-        .status(data.$metadata.httpStatusCode ?? 200)
-        .header('Content-Type', data.ContentType)
-        .header('Cache-Control', data.CacheControl)
-        .header('ETag', data.ETag)
-        .header('Last-Modified', data.LastModified)
-        .send(data.Body)
+  return response
+    .status(data.$metadata.httpStatusCode ?? 200)
+    .header('Content-Type', data.ContentType)
+    .header('Cache-Control', data.CacheControl)
+    .header('ETag', data.ETag)
+    .header('Last-Modified', data.LastModified)
+    .send(data.Body)
 }
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
@@ -82,7 +91,7 @@ export default async function routes(fastify: FastifyInstance) {
       },
     },
     async (request, response) => {
-     return requestHandler(request, response) 
+      return requestHandler(request, response)
     }
   )
 
@@ -94,13 +103,13 @@ export default async function routes(fastify: FastifyInstance) {
       schema: {
         params: getObjectParamsSchema,
         headers: { $ref: 'authSchema#' },
-        summary: "Deprecated (use /authenticated/bucketName/object instead): Retrieve an object",
+        summary: 'Deprecated (use /authenticated/bucketName/object instead): Retrieve an object',
         response: { '4xx': { $ref: 'errorSchema#' } },
         tags: ['object'],
       },
     },
     async (request, response) => {
-     return requestHandler(request, response) 
+      return requestHandler(request, response)
     }
   )
 }

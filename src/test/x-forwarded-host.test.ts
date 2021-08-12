05914049918d3558e544d168b70e9c46666e8cfe
Beng Eu <beng@bengeu.com>
2021-08-12 21:06:44 +0800

    fix: X-Forwarded-Host tests

--- src/test/x-forwarded-host.test.ts
+++ src/test/x-forwarded-host.test.ts
@@ -1,6 +1,10 @@
 'use strict'
 import app from '../app'
 import dotenv from 'dotenv'
+import * as project from '../utils/project'
+
+jest.mock('../utils/project')
+const mockedProject = project as jest.Mocked<typeof project>
 
 dotenv.config({ path: '.env.test' })
 
@@ -8,11 +12,12 @@ const ENV = process.env
 
 beforeEach(() => {
   process.env = { ...ENV }
+  process.env.X_FORWARDED_HOST_REGEXP = '^([a-z]{20})\\.supabase\\.(?:co|in|net)$'
 })
 
 describe('with X-Forwarded-Host header', () => {
   test('PostgREST URL is constructed using X-Forwarded-Host if regexp matches', async () => {
-    process.env.X_FORWARDED_HOST_REGEXP = '^([a-z]{20})\\.supabase\\.(?:co|in|net)$'
+    mockedProject.getAnonKey.mockResolvedValue('abc')
     const response = await app().inject({
       method: 'GET',
       url: `/bucket`,
@@ -21,13 +26,26 @@ describe('with X-Forwarded-Host header', () => {
         'x-forwarded-host': 'abcdefghijklmnopqrst.supabase.co',
       },
     })
+    expect(mockedProject.getAnonKey).toHaveBeenCalledWith('abcdefghijklmnopqrst')
     expect(response.statusCode).toBe(500)
     const responseJSON = JSON.parse(response.body)
     expect(responseJSON.message).toContain('http://abcdefghijklmnopqrst.supabase.co/rest/v1')
   })
 
-  test('PostgREST URL is not constructed using X-Forwarded-Host if regexp does not match', async () => {
-    process.env.X_FORWARDED_HOST_REGEXP = '^([a-z]{20})\\.supabase\\.(?:co|in|net)$'
+  test('Error is thrown if X-Forwarded-Host is not present', async () => {
+    const response = await app().inject({
+      method: 'GET',
+      url: `/bucket`,
+      headers: {
+        authorization: `Bearer ${process.env.AUTHENTICATED_KEY}`,
+      },
+    })
+    expect(response.statusCode).toBe(500)
+    const responseJSON = JSON.parse(response.body)
+    expect(responseJSON.message).toBe('X-Forwarded-Host header is not a string')
+  })
+
+  test('Error is thrown if X-Forwarded-Host does not match regexp', async () => {
     const response = await app().inject({
       method: 'GET',
       url: `/bucket`,
@@ -36,8 +54,8 @@ describe('with X-Forwarded-Host header', () => {
         'x-forwarded-host': 'abcdefghijklmnopqrst.supabase.com',
       },
     })
-    expect(response.statusCode).toBe(200)
+    expect(response.statusCode).toBe(500)
     const responseJSON = JSON.parse(response.body)
-    expect(responseJSON.length).toBe(5)
+    expect(responseJSON.message).toBe('X-Forwarded-Host header does not match regular expression')
   })
 })

69fdfded3d309d815712fa9b28d1695913af581f
Beng Eu <beng@bengeu.com>
2021-08-16 16:06:08 +0800

    test: forward 304 and If-Modified-Since/If-None-Match headers

--- src/test/object.test.ts
+++ src/test/object.test.ts
@@ -10,7 +10,7 @@ import { S3Backend } from '../backend/s3'
 dotenv.config({ path: '.env.test' })
 const { anonKey, serviceKey } = getConfig()
 
-beforeAll(() => {
+beforeEach(() => {
   jest.spyOn(S3Backend.prototype, 'getObject').mockResolvedValue({
     metadata: {
       httpStatusCode: 200,
@@ -61,6 +61,29 @@ describe('testing GET object', () => {
     })
     expect(response.statusCode).toBe(200)
     expect(S3Backend.prototype.getObject).toBeCalled()
+  })
+
+  test('forward 304 and If-Modified-Since/If-None-Match headers', async () => {
+    const mockGetObject = jest.spyOn(S3Backend.prototype, 'getObject')
+    mockGetObject.mockRejectedValue({
+      $metadata: {
+        httpStatusCode: 304,
+      },
+    })
+    const response = await app().inject({
+      method: 'GET',
+      url: '/object/authenticated/bucket2/authenticated/casestudy.png',
+      headers: {
+        authorization: `Bearer ${process.env.AUTHENTICATED_KEY}`,
+        'if-modified-since': 'Fri Aug 13 2021 00:00:00 GMT+0800 (Singapore Standard Time)',
+        'if-none-match': 'abc',
+      },
+    })
+    expect(response.statusCode).toBe(304)
+    expect(mockGetObject.mock.calls[0][2]).toMatchObject({
+      ifModifiedSince: 'Fri Aug 13 2021 00:00:00 GMT+0800 (Singapore Standard Time)',
+      ifNoneMatch: 'abc',
+    })
   })
 
   test('check if RLS policies are respected: anon user is not able to read authenticated resource', async () => {
@@ -895,6 +918,30 @@ describe('testing retrieving signed URL', () => {
       url: `/object/sign/${urlToSign}?token=${jwtToken}`,
     })
     expect(response.statusCode).toBe(200)
+  })
+
+  test('forward 304 and If-Modified-Since/If-None-Match headers', async () => {
+    const mockGetObject = jest.spyOn(S3Backend.prototype, 'getObject')
+    mockGetObject.mockRejectedValue({
+      $metadata: {
+        httpStatusCode: 304,
+      },
+    })
+    const urlToSign = 'bucket2/public/sadcat-upload.png'
+    const jwtToken = await signJWT({ url: urlToSign }, 100)
+    const response = await app().inject({
+      method: 'GET',
+      url: `/object/sign/${urlToSign}?token=${jwtToken}`,
+      headers: {
+        'if-modified-since': 'Fri Aug 13 2021 00:00:00 GMT+0800 (Singapore Standard Time)',
+        'if-none-match': 'abc',
+      },
+    })
+    expect(response.statusCode).toBe(304)
+    expect(mockGetObject.mock.calls[0][2]).toMatchObject({
+      ifModifiedSince: 'Fri Aug 13 2021 00:00:00 GMT+0800 (Singapore Standard Time)',
+      ifNoneMatch: 'abc',
+    })
   })
 
   test('get object without a token', async () => {

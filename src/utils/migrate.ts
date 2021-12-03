082abbe0c0cff739556825c01e320a9697f3590d
Beng Eu <beng@bengeu.com>
2021-12-03 12:56:20 +0800

    fix: do not track SQL statements when running migrations

--- src/utils/migrate.ts
+++ src/utils/migrate.ts
@@ -16,7 +16,9 @@ async function connectAndMigrate(
   const client = new Client(dbConfig)
   try {
     await client.connect()
+    await client.query('SET pg_stat_statements.track TO none')
     await migrate({ client }, migrationsDirectory)
+    await client.query('SET pg_stat_statements.track TO DEFAULT')
   } catch (error) {
     if (logOnError) {
       console.error('Migration error:', error.message)

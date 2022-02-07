48fb82be0efa45bf40e7fd8b3cf1b86001bc0e48
Div Arora <root@darora.com>
2022-02-07 14:27:29 +0800

    feat: more usable logging configuration
    - logs in prod are no longer limited to 'error' level
    - timestamps are outputted in a readable format rather than epoch ms
    
    additionally, prettier is updated to a newer version in keeping with
    our other codebases.

--- src/server.ts
+++ src/server.ts
@@ -1,51 +1,54 @@
 import { FastifyInstance } from 'fastify'
 import { IncomingMessage, Server, ServerResponse } from 'http'
+import pino from 'pino'
+
 import build from './app'
 import buildAdmin from './admin-app'
 import { getConfig } from './utils/config'
 import { runMultitenantMigrations, runMigrations } from './utils/migrate'
 import { cacheTenantConfigsFromDbAndRunMigrations } from './utils/tenant'
 
-const loggerConfig = {
-  prettyPrint: true,
-  level: 'info',
-}
+let logger = pino({
+  formatters: {
+    level(label) {
+      return { level: label }
+    },
+  },
+  timestamp: pino.stdTimeFunctions.isoTime,
+})
+
 const exposeDocs = true
-if (process.env.NODE_ENV === 'production') {
-  loggerConfig.prettyPrint = false
-  loggerConfig.level = 'error'
-}
 
-;(async () => {
-  const { isMultitenant } = getConfig()
-  if (isMultitenant) {
-    await runMultitenantMigrations()
-    await cacheTenantConfigsFromDbAndRunMigrations()
+  ; (async () => {
+    const { isMultitenant } = getConfig()
+    if (isMultitenant) {
+      await runMultitenantMigrations()
+      await cacheTenantConfigsFromDbAndRunMigrations()
 
-    const adminApp: FastifyInstance<Server, IncomingMessage, ServerResponse> = buildAdmin({
-      logger: loggerConfig,
+      const adminApp: FastifyInstance<Server, IncomingMessage, ServerResponse> = buildAdmin({
+        logger,
+      })
+
+      try {
+        await adminApp.listen(5001, '0.0.0.0')
+      } catch (err) {
+        adminApp.log.error(err)
+        process.exit(1)
+      }
+    } else {
+      await runMigrations()
+    }
+
+    const app: FastifyInstance<Server, IncomingMessage, ServerResponse> = build({
+      logger,
+      exposeDocs,
     })
 
-    try {
-      await adminApp.listen(5001, '0.0.0.0')
-    } catch (err) {
-      adminApp.log.error(err)
-      process.exit(1)
-    }
-  } else {
-    await runMigrations()
-  }
-
-  const app: FastifyInstance<Server, IncomingMessage, ServerResponse> = await build({
-    logger: loggerConfig,
-    exposeDocs,
-  })
-
-  app.listen(5000, '0.0.0.0', (err, address) => {
-    if (err) {
-      console.error(err)
-      process.exit(1)
-    }
-    console.log(`Server listening at ${address}`)
-  })
-})()
+    app.listen(5000, '0.0.0.0', (err, address) => {
+      if (err) {
+        console.error(err)
+        process.exit(1)
+      }
+      console.log(`Server listening at ${address}`)
+    })
+  })()

403287bd7350d8213cc692d3132daf151abe90fe
Div Arora <root@darora.com>
2022-06-15 14:38:04 +0800

    feat: expose metrics endpoint

--- src/admin-app.ts
+++ src/admin-app.ts
@@ -1,10 +1,16 @@
 import fastify, { FastifyInstance, FastifyServerOptions } from 'fastify'
+import { default as metrics } from 'fastify-metrics'
 import tenantRoutes from './routes/tenant'
 
 const build = (opts: FastifyServerOptions = {}): FastifyInstance => {
   const app = fastify(opts)
   app.register(tenantRoutes, { prefix: 'tenants' })
-  app.get('/status', async (request, response) => response.status(200).send())
+  app.register(metrics, {
+    endpoint: '/metrics',
+    enableRouteMetrics: false,
+    blacklist: ['nodejs_version_info', 'process_start_time_seconds'],
+  })
+  app.get('/status', async (_, response) => response.status(200).send())
   return app
 }
 

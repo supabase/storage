9bf39cd2b5f897b55b60a4af7cadc8b5f53b33af
Benjamin Tan <benjamin@dev.ofcr.se>
2022-08-01 13:35:29 +0800

    fix: prevent creation of objects with no name

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -90,5 +90,5 @@ export function normalizeContentType(contentType: string | undefined): string | 
 export function isValidKey(key: string): boolean {
   // only allow s3 safe characters and characters which require special handling for now
   // https://docs.aws.amazon.com/AmazonS3/latest/userguide/object-keys.html
-  return /^(\w|\/|!|-|\.|\*|'|\(|\)| |&|\$|@|=|;|:|\+|,|\?)*$/.test(key)
+  return key.length > 0 && /^(\w|\/|!|-|\.|\*|'|\(|\)| |&|\$|@|=|;|:|\+|,|\?)*$/.test(key)
 }

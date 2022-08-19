84b85338a8a3a7c705922ea300a6a1c399a8b32c
Inian <inian1234@gmail.com>
2022-08-19 16:51:10 +0800

    add example for auth header

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -2,7 +2,12 @@ export const authSchema = {
   $id: 'authSchema',
   type: 'object',
   properties: {
-    authorization: { type: 'string' },
+    authorization: {
+      type: 'string',
+      examples: [
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs',
+      ],
+    },
   },
   required: ['authorization'],
 } as const

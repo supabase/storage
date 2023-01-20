284b82f9aac2cb02363565942a45097a58678897
fenos <fabri.feno@gmail.com>
2023-01-20 11:53:37 +0000

    fix: opt-in support for avif

--- src/http/schemas/transformations.ts
+++ src/http/schemas/transformations.ts
@@ -2,6 +2,6 @@ export const transformationOptionsSchema = {
   height: { type: 'integer', examples: [100], minimum: 0 },
   width: { type: 'integer', examples: [100], minimum: 0 },
   resize: { type: 'string', enum: ['cover', 'contain', 'fill'] },
-  format: { type: 'string', enum: ['origin'] },
+  format: { type: 'string', enum: ['origin', 'avif'] },
   quality: { type: 'integer', minimum: 20, maximum: 100 },
 } as const

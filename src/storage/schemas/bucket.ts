c43ba53f96ada8ebf456522504e488408b74f7d7
Inian <inian1234@gmail.com>
2023-04-09 13:06:09 +0800

    feat: return size and mime types in bucket GET

--- src/storage/schemas/bucket.ts
+++ src/storage/schemas/bucket.ts
@@ -19,6 +19,9 @@ export const bucketSchema = {
     {
       id: 'bucket2',
       name: 'bucket2',
+      public: false,
+      file_size_limit: 1000000,
+      allowed_mime_types: ['image/png', 'image/jpeg'],
       owner: '4d56e902-f0a0-4662-8448-a4d9e643c142',
       created_at: '2021-02-17T04:43:32.770206+00:00',
       updated_at: '2021-02-17T04:43:32.770206+00:00',

7abfce4634e66cf60c87946265fa023dff7d1d10
egor-romanov <egor.romanov@gmail.com>
2023-12-04 22:17:18 +0400

    feat: add gravity param for image transformation

--- src/http/schemas/transformations.ts
+++ src/http/schemas/transformations.ts
@@ -4,4 +4,7 @@ export const transformationOptionsSchema = {
   resize: { type: 'string', enum: ['cover', 'contain', 'fill'] },
   format: { type: 'string', enum: ['origin', 'avif'] },
   quality: { type: 'integer', minimum: 20, maximum: 100 },
+  gravity: { type: 'string', enum: ['no', 'so', 'ea', 'we', 'noea', 'nowe', 'soea', 'sowe', 'ce', 'sm', 'fp'] },
+  x_offset: { type: 'number', examples: [0.1, 100], minimum: 0 },
+  y_offset: { type: 'number', examples: [0.1, 100], minimum: 0 },
 } as const

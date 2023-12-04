29b541ecbc13ed0ac5e929576b3c7b9ec52e97e6
egor-romanov <egor.romanov@gmail.com>
2023-12-04 22:26:18 +0400

    fix: offset can be negative

--- src/http/schemas/transformations.ts
+++ src/http/schemas/transformations.ts
@@ -5,6 +5,6 @@ export const transformationOptionsSchema = {
   format: { type: 'string', enum: ['origin', 'avif'] },
   quality: { type: 'integer', minimum: 20, maximum: 100 },
   gravity: { type: 'string', enum: ['no', 'so', 'ea', 'we', 'noea', 'nowe', 'soea', 'sowe', 'ce', 'sm', 'fp'] },
-  x_offset: { type: 'number', examples: [0.1, 100], minimum: 0 },
-  y_offset: { type: 'number', examples: [0.1, 100], minimum: 0 },
+  x_offset: { type: 'number', examples: [0.1, 100] },
+  y_offset: { type: 'number', examples: [0.1, -100] },
 } as const

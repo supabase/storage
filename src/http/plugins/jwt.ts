640443867bc41148d0e6f2ee78a41221592f041d
fenos <fabri.feno@gmail.com>
2024-09-05 13:17:03 +0200

    feat: handle authentication on top level routes

--- src/http/plugins/jwt.ts
+++ src/http/plugins/jwt.ts
@@ -7,9 +7,14 @@ import { ERRORS } from '@internal/errors'
 
 declare module 'fastify' {
   interface FastifyRequest {
+    isAuthenticated: boolean
     jwt: string
     jwtPayload?: JwtPayload & { role?: string }
     owner?: string
+  }
+
+  interface FastifyContextConfig {
+    allowInvalidJwt?: boolean
   }
 }
 
@@ -22,13 +27,25 @@ export const jwt = fastifyPlugin(async (fastify) => {
   fastify.addHook('preHandler', async (request, reply) => {
     request.jwt = (request.headers.authorization || '').replace(BEARER, '')
 
+    if (!request.jwt && request.routeConfig.allowInvalidJwt) {
+      request.jwtPayload = { role: 'anon' }
+      request.isAuthenticated = false
+      return
+    }
+
     const { secret, jwks } = await getJwtSecret(request.tenantId)
 
     try {
       const payload = await verifyJWT(request.jwt, secret, jwks || null)
       request.jwtPayload = payload
       request.owner = payload.sub
+      request.isAuthenticated = true
     } catch (err: any) {
+      request.isAuthenticated = false
+
+      if (request.routeConfig.allowInvalidJwt) {
+        return
+      }
       throw ERRORS.AccessDenied(err.message, err)
     }
   })

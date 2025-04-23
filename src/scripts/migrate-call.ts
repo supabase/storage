5fc75679718f2744fc7e49be9296299d2b8606ea
Han Qiao <qiao@supabase.io>
2025-04-23 18:26:36 +0800

    fix: optionally freeze migrations when running migrate-call script

--- src/scripts/migrate-call.ts
+++ src/scripts/migrate-call.ts
@@ -5,5 +5,6 @@ import { runMigrationsOnTenant } from '@internal/database/migrations'
 ;(async () => {
   await runMigrationsOnTenant({
     databaseUrl: process.env.DATABASE_URL as string,
+    upToMigration: process.env.DB_MIGRATIONS_FREEZE_AT,
   })
 })()

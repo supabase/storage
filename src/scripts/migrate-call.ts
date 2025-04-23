b9fc3c93bdb775c9a3650823dea2e07205e5a46d
Han Qiao <qiao@supabase.io>
2025-04-23 18:30:39 +0800

    chore: use get config in migrate call script

--- src/scripts/migrate-call.ts
+++ src/scripts/migrate-call.ts
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-import { runMigrationsOnTenant } from '@internal/database/migrations'
+import { getConfig, runMigrationsOnTenant } from '@internal/database/migrations'
 ;(async () => {
+  const { databaseURL, dbMigrationFreezeAt } = getConfig()
   await runMigrationsOnTenant({
-    databaseUrl: process.env.DATABASE_URL as string,
-    upToMigration: process.env.DB_MIGRATIONS_FREEZE_AT,
+    databaseUrl: databaseURL,
+    upToMigration: dbMigrationFreezeAt,
   })
 })()

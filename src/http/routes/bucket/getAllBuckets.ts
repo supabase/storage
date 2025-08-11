9e7e9ac2def461026d8372c704c5dc260470b383
Lenny <itslenny@users.noreply.github.com>
2025-08-11 06:06:36 -0400

    fix: exclude bucket type in response for storage-py < 0.12.1 and supabase-py < 2.18.0 (#746)

--- src/http/routes/bucket/getAllBuckets.ts
+++ src/http/routes/bucket/getAllBuckets.ts
@@ -4,7 +4,7 @@ import { createDefaultSchema } from '../../routes-helper'
 import { AuthenticatedRequest } from '../../types'
 import { bucketSchema } from '@storage/schemas'
 import { ROUTE_OPERATIONS } from '../operations'
-import { isPythonClientBefore } from '@storage/limits'
+import { isClientVersionBefore } from '@storage/limits'
 
 const successResponseSchema = {
   type: 'array',
@@ -63,11 +63,16 @@ export default async function routes(fastify: FastifyInstance) {
       // Detects user agents that support the type property in bucket list response
       // storage-py < v0.12.1 throws fatal error if type property is present
       // type property added in v0.12.1 -- https://github.com/supabase/storage-py/releases/tag/v0.12.1
-      const includeBucketType = !isPythonClientBefore(request.headers['user-agent'] || '', '0.12.1')
+      // added to supabase-py in v2.18.0 -- https://github.com/supabase/supabase-py/releases/tag/v2.18.0
+      const clientInfo = (request.headers['x-client-info'] as string) || ''
+      const userAgent = request.headers['user-agent'] || ''
+      const omitBucketType =
+        isClientVersionBefore('supabase-py', clientInfo, '2.18.0') ||
+        isClientVersionBefore('storage3', userAgent, '0.12.1')
 
       const results = await request.storage.listBuckets(
         'id, name, public, owner, created_at, updated_at, file_size_limit, allowed_mime_types' +
-          (includeBucketType ? ', type' : ''),
+          (omitBucketType ? '' : ', type'),
         { limit, offset, sortColumn, sortOrder, search }
       )
 

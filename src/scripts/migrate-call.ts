0aea40b80fe4fdbd25cf4c274d757c29c5ec8808
Qiao Han <qiao@supabase.io>
2025-10-09 00:30:03 +0800

    chore: ignore eslint error in main

--- src/scripts/migrate-call.ts
+++ src/scripts/migrate-call.ts
@@ -3,6 +3,7 @@ dotenv.config()
 
 import { runMigrationsOnTenant } from '@internal/database/migrations'
 import { getConfig } from '../config'
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 ;(async () => {
   const { databaseURL, dbMigrationFreezeAt } = getConfig()
   await runMigrationsOnTenant({

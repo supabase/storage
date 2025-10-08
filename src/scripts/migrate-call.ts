3562ba0f445a351b40e6b0d22f18b506b9d2d031
Han Qiao <qiao@supabase.io>
2025-10-09 00:53:26 +0800

    fix: ensure failure exit code is properly reported (#778)
    * fix: ensure failure exit code is properly reported
    
    * chore: ignore eslint error in main
    
    * chore: try voiding the promise

--- src/scripts/migrate-call.ts
+++ src/scripts/migrate-call.ts
@@ -3,10 +3,10 @@ dotenv.config()
 
 import { runMigrationsOnTenant } from '@internal/database/migrations'
 import { getConfig } from '../config'
-;(async () => {
+void (async () => {
   const { databaseURL, dbMigrationFreezeAt } = getConfig()
   await runMigrationsOnTenant({
     databaseUrl: databaseURL,
     upToMigration: dbMigrationFreezeAt,
   })
-})().catch(console.error)
+})()

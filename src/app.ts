cce047b6dca69c987a16976af739175365b1edd5
Lenny <itslenny@users.noreply.github.com>
2025-12-09 09:58:13 -0600

    feat: add version endpoint to admin api (#811)

--- src/app.ts
+++ src/app.ts
@@ -54,7 +54,7 @@ const build = (opts: buildOpts = {}): FastifyInstance => {
   app.register(plugins.tenantId)
   app.register(plugins.metrics({ enabledEndpoint: !isMultitenant }))
   app.register(plugins.tracing)
-  app.register(plugins.logRequest({ excludeUrls: ['/status', '/metrics', '/health'] }))
+  app.register(plugins.logRequest({ excludeUrls: ['/status', '/metrics', '/health', '/version'] }))
   app.register(routes.tus, { prefix: 'upload/resumable' })
   app.register(routes.bucket, { prefix: 'bucket' })
   app.register(routes.object, { prefix: 'object' })

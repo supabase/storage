1580e63793a94928a14b49410cf03f963edb52b2
fenos <fabri.feno@gmail.com>
2026-02-18 16:03:16 +0100

    feat: support pgbouncer for multitenant database

--- src/config.ts
+++ src/config.ts
@@ -65,6 +65,7 @@ type StorageConfigType = {
   storageFileEtagAlgorithm: 'mtime' | 'md5'
   storageS3InternalTracesEnabled?: boolean
   storageS3MaxSockets: number
+  storageS3DisableChecksum: boolean
   storageS3UploadQueueSize: number
   storageS3Bucket: string
   storageS3Endpoint?: string
@@ -77,6 +78,8 @@ type StorageConfigType = {
   jwtCachingEnabled: boolean
   jwtJWKS?: JwksConfig
   multitenantDatabaseUrl?: string
+  multitenantDatabasePoolUrl?: string
+  multitenantMaxConnections: number
   dbAnonRole: string
   dbAuthenticatedRole: string
   dbServiceRole: string
@@ -357,6 +360,7 @@ export function getConfig(options?: { reload?: boolean }): StorageConfigType {
       getOptionalConfigFromEnv('STORAGE_S3_MAX_SOCKETS', 'GLOBAL_S3_MAX_SOCKETS') || '200',
       10
     ),
+    storageS3DisableChecksum: getOptionalConfigFromEnv('STORAGE_S3_DISABLE_CHECKSUM') === 'true',
     storageS3UploadQueueSize:
       envNumber(getOptionalConfigFromEnv('STORAGE_S3_UPLOAD_QUEUE_SIZE')) ?? 2,
     storageS3InternalTracesEnabled:
@@ -389,6 +393,17 @@ export function getConfig(options?: { reload?: boolean }): StorageConfigType {
     multitenantDatabaseUrl: getOptionalConfigFromEnv(
       'DATABASE_MULTITENANT_URL',
       'MULTITENANT_DATABASE_URL'
+    ),
+    multitenantDatabasePoolUrl: getOptionalConfigFromEnv(
+      'DATABASE_MULTITENANT_POOL_URL',
+      'MULTITENANT_DATABASE_POOL_URL'
+    ),
+    multitenantMaxConnections: envNumber(
+      getOptionalConfigFromEnv(
+        'DATABASE_MULTITENANT_MAX_CONNECTIONS',
+        'MULTITENANT_DATABASE_MAX_CONNECTIONS'
+      ),
+      10
     ),
     databaseSSLRootCert: getOptionalConfigFromEnv('DATABASE_SSL_ROOT_CERT'),
     databaseURL: getOptionalIfMultitenantConfigFromEnv('DATABASE_URL') || '',
@@ -602,13 +617,13 @@ export function getConfig(options?: { reload?: boolean }): StorageConfigType {
   return config
 }
 
-function envNumber(value: string | undefined): number | undefined {
+function envNumber(value: string | undefined, defaultValue?: number): number | undefined {
   if (!value) {
-    return undefined
+    return defaultValue
   }
   const parsed = parseInt(value, 10)
   if (isNaN(parsed)) {
-    return undefined
+    return defaultValue
   }
   return parsed
 }

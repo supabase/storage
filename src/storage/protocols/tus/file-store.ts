8a08928f9fc7c2c179c077a4176d26a3e842cbf9
ferhat elmas <elmas.ferhat@gmail.com>
2026-03-30 15:01:00 +0200

    fix: drop fs-extra for native fs/promises
    https://github.com/es-tooling/module-replacements/blob/main/docs/modules/fs-extra.md
    
    Signed-off-by: ferhat elmas <elmas.ferhat@gmail.com>

--- src/storage/protocols/tus/file-store.ts
+++ src/storage/protocols/tus/file-store.ts
@@ -1,6 +1,6 @@
+import { ensureFile } from '@internal/fs'
 import { Configstore, FileStore as TusFileStore } from '@tus/file-store'
 import { Upload } from '@tus/server'
-import fsExtra from 'fs-extra'
 import path from 'path'
 import { FileBackend } from '../../backend'
 
@@ -20,7 +20,7 @@ export class FileStore extends TusFileStore {
 
   async create(file: Upload): Promise<Upload> {
     const filePath = path.join(this.options.directory, file.id)
-    await fsExtra.ensureFile(filePath)
+    await ensureFile(filePath)
 
     await this.fileAdapter.setFileMetadata(filePath, {
       cacheControl: file.metadata?.cacheControl || '',

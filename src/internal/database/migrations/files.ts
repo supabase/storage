aec99fe230203dc6851836352a17b86ae8d9d55c
ferhat elmas <elmas.ferhat@gmail.com>
2026-04-15 18:49:34 +0200

    chore: simplify migration memoize
    Signed-off-by: ferhat elmas <elmas.ferhat@gmail.com>

--- src/internal/database/migrations/files.ts
+++ src/internal/database/migrations/files.ts
@@ -2,24 +2,42 @@ import { DBMigration } from '@internal/database/migrations/types'
 import { ERRORS } from '@internal/errors'
 import { loadMigrationFiles } from 'postgres-migrations'
 import { getConfig } from '../../../config'
-import { memoizePromise } from './memoize'
 
 const { dbMigrationFreezeAt } = getConfig()
 
-export const loadMigrationFilesCached = memoizePromise(loadMigrationFiles)
+const migrationFilesCache = new Map<string, ReturnType<typeof loadMigrationFiles>>()
 
-export const localMigrationFiles = () => loadMigrationFiles('./migrations/tenant')
+export function loadMigrationFilesCached(directory: string) {
+  let promise = migrationFilesCache.get(directory)
+
+  if (!promise) {
+    promise = loadMigrationFiles(directory).catch((error) => {
+      migrationFilesCache.delete(directory)
+      throw error
+    })
+    migrationFilesCache.set(directory, promise)
+  }
+
+  return promise
+}
+
+export const localMigrationFiles = () => loadMigrationFilesCached('./migrations/tenant')
 
 export async function lastLocalMigrationName() {
-  const migrations = await loadMigrationFilesCached('./migrations/tenant')
+  const migrations = await localMigrationFiles()
+  const latestMigration = migrations.at(-1)
+
+  if (!latestMigration) {
+    throw ERRORS.InternalError(undefined, 'No local migrations found')
+  }
 
   if (!dbMigrationFreezeAt) {
-    return migrations[migrations.length - 1].name as keyof typeof DBMigration
+    return latestMigration.name as keyof typeof DBMigration
   }
 
-  const migrationIndex = migrations.findIndex((m) => m.name === dbMigrationFreezeAt)
-  if (migrationIndex === -1) {
+  const frozenMigration = migrations.find((m) => m.name === dbMigrationFreezeAt)
+  if (!frozenMigration) {
     throw ERRORS.InternalError(undefined, `Migration ${dbMigrationFreezeAt} not found`)
   }
-  return migrations[migrationIndex].name as keyof typeof DBMigration
+  return frozenMigration.name as keyof typeof DBMigration
 }

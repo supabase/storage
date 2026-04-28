d0f7d26ce45d97323ee60d3ded4a54b63f9611cc
Lenny <itslenny@users.noreply.github.com>
2026-04-28 17:29:50 -0400

    fix: generate swagger ui with script (#1064)
    * fix: generate swagger ui with script
    
    * add error handling

---
+++ src/scripts/export-docs-ui.ts
@@ -0,0 +1,26 @@
+import { promises as fs } from 'fs'
+import { resolve } from 'path'
+
+function swaggerInitializer(targetUrl: string) {
+  return `window.onload = function () {
+    const config = {
+      dom_id: '#swagger-ui',
+      deepLinking: true,
+      url: '${targetUrl}'
+    }
+    const ui = SwaggerUIBundle(config)
+  }`
+}
+
+;(async () => {
+  const targetPath = 'swagger-ui'
+  const swaggerPath = resolve('node_modules', '@fastify', 'swagger-ui', 'static')
+  const specFile = 'api.json'
+  const specPath = resolve('static', specFile)
+  await fs.cp(swaggerPath, targetPath, { recursive: true })
+  await fs.cp(specPath, resolve(targetPath, specFile))
+  await fs.writeFile(resolve(targetPath, 'swagger-initializer.js'), swaggerInitializer(specFile))
+})().catch((e) => {
+  console.error(e)
+  process.exit(1)
+})

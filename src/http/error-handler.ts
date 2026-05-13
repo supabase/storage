8e143ad6db9b0fce7998a5fd8eca4b7539496085
Lenny <lenny@itslennysfault.com>
2026-05-13 12:58:23 -0400

    fix: correctly categorize errors related to saturated project databases

--- src/http/error-handler.ts
+++ src/http/error-handler.ts
@@ -33,6 +33,8 @@ export const setErrorHandler = (
         'no more connections allowed',
         'sorry, too many clients already',
         'server login has been failing, try again later',
+        'server login has been failing, cached error: connect timeout (server_login_retry)',
+        'server login has been failing, cached error: the database system is not accepting connections (server_login_retry)',
       ].some((msg) => (error as DatabaseError).message.includes(msg))
     ) {
       return reply.status(429).send(

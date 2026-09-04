10314a1667d64cd93f98cdffd96addaa816ccec5
Tyler Hillery <tyhillery@gmail.com>
2026-09-04 08:31:12 -0500

    fix: respect sort order for sortAfter

--- src/storage/database/pg.ts
+++ src/storage/database/pg.ts
@@ -740,7 +740,7 @@ export class StoragePgDB implements Database {
 
         if (options?.startAfter && !options?.nextToken) {
           values.push(options.startAfter)
-          conditions.push(`name COLLATE "C" > $${values.length}`)
+          conditions.push(`name COLLATE "C" ${pageOperator} $${values.length}`)
         }
 
         if (options?.nextToken) {
